7e373c5369c1c3c9f2e80a6428e9d5eeae5114ab
amzy-0 <azymiamin@gmail.com>
2021-04-16 11:59:41 +0430

    update controller added

--- core_pck3r.py
+++ core_pck3r.py
@@ -46,7 +46,14 @@ for i in range(argc):
             
             # pck3r updator
             elif argv[1] == 'update' and argc == 2:
-                syscall('git pull')
+                
+                if (syscall('ls %s/.pck3r > /dev/null 2>&1' % getenv('HOME')))==0:
+                    chdir('%s/.pck3r' % getenv('HOME'))
+                    syscall('git pull')
+
+                else:
+                    print('%s%sPck3r is not installed%s'
+                    %(stuff.sysERR(), stuff.RED, stuff.NRM))
 
             # if argument 1 equal to "help"
             # like -> $ pck3r help
